c81b009c47ed7027ae91e727ee9cfe611f8af60f
Peter Good <peterwgood77@gmail.com>
2026-03-31 09:22:28 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Career-Tracker/', // This matches your repository name exactly
+  base: '/Career-Tracker/', // This is the missing piece!
 })
